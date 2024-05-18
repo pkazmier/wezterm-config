953260eb1bea95fd7226d6dd3cb4725aa3194de9
Pete Kazmier <pete@kazmier.com>
2024-05-18 08:37:03 -0400

    feat: add M PLUS Code Latin font
    This is a nice monospaced font. It's narrow, but still very readable.
    I do wish it had an italic version though.

--- fonts/mplus.lua
+++ fonts/mplus.lua
@@ -1,17 +1,18 @@
 local wezterm = require("wezterm")
 local M = {}
+local name = "M PLUS Code Latin"
 
 M.init = function()
-  return "PragmataPro"
+  return name
 end
 
 M.activate = function(config)
-  config.font = wezterm.font("PragmataProMonoLiga Nerd Font")
+  config.font = wezterm.font(name)
   -- config.freetype_load_target = "Light"
   -- config.freetype_render_target = "HorizontalLcd"
-  config.font_size = 15
-  config.line_height = 1.35
-  config.harfbuzz_features = { "ss13" }
+  config.font_size = 15.0
+  config.line_height = 1.2
+  config.harfbuzz_features = {}
   config.font_rules = {}
 end
 
