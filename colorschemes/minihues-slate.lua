590707bb05aeb610bb6bc10cc2d3e8ba33684ab2
Pete Kazmier <pete@kazmier.com>
2024-05-19 11:18:53 -0400

    feat: a better slate theme

--- colorschemes/minihues-slate.lua
+++ colorschemes/minihues-slate.lua
@@ -1,5 +1,5 @@
 local M = {}
-local name = "MiniHues Slate"
+local name = "MiniHues - slate"
 
 M.init = function()
   return name
@@ -8,46 +8,45 @@ end
 M.activate = function(config)
   config.color_schemes = config.color_schemes or {}
   config.color_schemes[name] = {
-    ansi = { "#1c2231", "#f7b3b3", "#9fd7af", "#cbcc8f", "#9acef8", "#e8b4dd", "#86d7da", "#c4c7cd" },
-    background = "#1c2231",
-    brights = { "#353b4c", "#f7b3b3", "#9fd7af", "#cbcc8f", "#9acef8", "#e8b4dd", "#86d7da", "#a7aab0" },
-    compose_cursor = "#edbc91",
+    ansi = { "#1a2331", "#f6b2ba", "#a6d5a8", "#d1c98c", "#94cff4", "#e3b5e3", "#87d8d3", "#c3c7cd" },
+    background = "#1a2331",
+    brights = { "#333c4c", "#f6b2ba", "#a6d5a8", "#d1c98c", "#94cff4", "#e3b5e3", "#87d8d3", "#a6aab0" },
+    compose_cursor = "#f0ba94",
     copy_mode_active_highlight_bg = {
-      Color = "#1c2231",
+      Color = "#1a2331",
     },
     copy_mode_active_highlight_fg = {
-      Color = "#a7aab0",
+      Color = "#a6aab0",
     },
     copy_mode_inactive_highlight_bg = {
-      Color = "#9fd7af",
+      Color = "#a6d5a8",
     },
     copy_mode_inactive_highlight_fg = {
-      Color = "#1c2231",
+      Color = "#1a2331",
     },
-    cursor_bg = "#9acef8",
-    cursor_border = "#9acef8",
-    cursor_fg = "#002f4e",
-    foreground = "#c4c7cd",
+    cursor_bg = "#94cff4",
+    cursor_border = "#94cff4",
+    cursor_fg = "#003751",
+    foreground = "#c3c7cd",
     indexed = {
-      [136] = "#edbc91",
+      [136] = "#f0ba94",
     },
     quick_select_label_bg = {
-      Color = "#edbc91",
+      Color = "#f0ba94",
     },
     quick_select_label_fg = {
-      Color = "#1c2231",
+      Color = "#1a2331",
     },
     quick_select_match_bg = {
-      Color = "#353b4c",
+      Color = "#333c4c",
     },
     quick_select_match_fg = {
-      Color = "#9acef8",
+      Color = "#94cff4",
     },
-    scrollbar_thumb = "#353b4c",
-    selection_bg = "#4f5667",
-    split = "#4f5667",
+    scrollbar_thumb = "#333c4c",
+    selection_bg = "#4c5767",
+    split = "#4c5767",
   }
-
   config.color_scheme = name
 end
 
