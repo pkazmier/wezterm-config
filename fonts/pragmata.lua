0f19ea1a7b10d9d1d2da61df2f2ad0acfbc3e5cc
Pete Kazmier <pete@kazmier.com>
2024-05-30 09:43:27 -0400

    feat: make default PragmataPro font size 14pt

--- fonts/pragmata.lua
+++ fonts/pragmata.lua
@@ -9,8 +9,8 @@ M.activate = function(config)
   config.font = wezterm.font("PragmataProMonoLiga Nerd Font")
   -- config.freetype_load_target = "Light"
   -- config.freetype_render_target = "HorizontalLcd"
-  config.font_size = 15
-  config.line_height = 1.35
+  config.font_size = 14
+  config.line_height = 1.3
   config.harfbuzz_features = { "ss13" }
   config.font_rules = {}
 end
