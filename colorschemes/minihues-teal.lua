255dcadafb5199a10c20d10d971b69e365002359
Pete Kazmier <pete@kazmier.com>
2024-06-08 10:28:45 -0400

    fix: renamed green theme to teal

--- colorschemes/minihues-teal.lua
+++ colorschemes/minihues-teal.lua
@@ -8,44 +8,44 @@ end
 M.activate = function(config)
   config.color_schemes = config.color_schemes or {}
   config.color_schemes[name] = {
-    ansi = { "#303d4c", "#f4b1bf", "#abd4a3", "#d6c78b", "#90d1f1", "#deb6e7", "#88d8ce", "#888c92" },
-    background = "#172331",
-    brights = { "#4a5767", "#f4b1bf", "#abd4a3", "#d6c78b", "#90d1f1", "#deb6e7", "#88d8ce", "#a5aab0" },
-    compose_cursor = "#f2b898",
+    ansi = { "#2b3f48", "#ecb3d5", "#c3ce92", "#e7bf8d", "#86d6e1", "#ccbcf5", "#97d7b7", "#868d91" },
+    background = "#12252e",
+    brights = { "#455964", "#ecb3d5", "#c3ce92", "#e7bf8d", "#86d6e1", "#ccbcf5", "#97d7b7", "#a3abaf" },
+    compose_cursor = "#f6b3ab",
     copy_mode_active_highlight_bg = {
-      Color = "#172331",
+      Color = "#12252e",
     },
     copy_mode_active_highlight_fg = {
-      Color = "#a5aab0",
+      Color = "#a3abaf",
     },
     copy_mode_inactive_highlight_bg = {
-      Color = "#abd4a3",
+      Color = "#c3ce92",
     },
     copy_mode_inactive_highlight_fg = {
-      Color = "#172331",
+      Color = "#12252e",
     },
-    cursor_bg = "#90d1f1",
-    cursor_border = "#90d1f1",
-    cursor_fg = "#003c52",
-    foreground = "#c2c7cd",
+    cursor_bg = "#86d6e1",
+    cursor_border = "#86d6e1",
+    cursor_fg = "#004a52",
+    foreground = "#c0c8cc",
     indexed = {
-      [136] = "#f2b898",
+      [136] = "#f6b3ab",
     },
     quick_select_label_bg = {
-      Color = "#f2b898",
+      Color = "#f6b3ab",
     },
     quick_select_label_fg = {
-      Color = "#172331",
+      Color = "#12252e",
     },
     quick_select_match_bg = {
-      Color = "#303d4c",
+      Color = "#2b3f48",
     },
     quick_select_match_fg = {
-      Color = "#90d1f1",
+      Color = "#86d6e1",
     },
-    scrollbar_thumb = "#303d4c",
-    selection_bg = "#4a5767",
-    split = "#4a5767",
+    scrollbar_thumb = "#2b3f48",
+    selection_bg = "#455964",
+    split = "#455964",
   }
   config.color_scheme = name
 end
