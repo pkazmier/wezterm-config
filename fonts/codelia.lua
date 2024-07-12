f9e01e42c86970b99af59b5ce582b27f3f1a7bea
Pete Kazmier <pete@kazmier.com>
2024-07-12 07:49:46 -0400

    feat: add codelia to the font collection

--- fonts/codelia.lua
+++ fonts/codelia.lua
@@ -2,15 +2,16 @@ local wezterm = require("wezterm")
 local M = {}
 
 M.init = function()
-  return "Comic Code"
+  return "Codelia"
 end
 
 M.activate = function(config)
-  config.font = wezterm.font("Comic Code Ligatures")
+  config.font = wezterm.font("Codelia Ligatures")
+
   -- config.freetype_load_target = "Light"
   -- config.freetype_render_target = "HorizontalLcd"
   config.font_size = 13.0
-  config.line_height = 1.3
+  config.line_height = 1.2
   config.harfbuzz_features = {}
   config.font_rules = {}
 end
